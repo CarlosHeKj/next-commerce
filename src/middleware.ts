a85e52d357a08c062064827967fac5885fd65cd1
CarlosHeKj <carloslouise2015@gmail.com>
2024-09-26 17:15:20 -0300

    Ajeitando logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/sign-in(.*)',
     '/sign-up(.*)',
+    '/api/webhooks/user',
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
